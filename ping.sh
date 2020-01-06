bd37516fd4e887df4639037053a8b6a672246f67
ITTech37 <45128030+ITTech37@users.noreply.github.com>
2020-01-06 21:58:34 +0100

    Add files via upload

---
+++ ping.sh
@@ -0,0 +1,14 @@
+﻿### Variable ###
+pingCount=5
+###
+
+# Read the user input
+read -p "Enter target hostname or IP (default google.com) : " hostip
+
+# If hostip variable is empty, use google.com. Then, use user choice.
+if [ -z $hostip ]
+then
+	ping -c $pingCount google.com
+else
+	ping -c $pingCount $hostip
+fi
